683f4237c14a9a28edf56dc1a97fd3c8e4b69e25
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-07 19:28:58 -0600

    feat: return 404

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,24 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    let contents = fs::read_to_string("index.html").unwrap();
 
     stream.read(&mut buffer).unwrap();
 
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n"; // return a reference to a byte string
 
-    // HTTP_Version Status_Code Reason_Phrase
-    // headers CRLF
-    // message_body
-    //
-    // ex: HTTP/1.1 200 OK\r\n\r\n
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
 
-    // HTTP response
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    let response = format!("{}{}", response, contents);
+        stream.write(response.as_bytes()).unwrap();
 
-    stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
 
-    stream.flush().unwrap();
+        stream.write(response.as_bytes()).unwrap();
+
+        stream.flush().unwrap();
+    };
 }
